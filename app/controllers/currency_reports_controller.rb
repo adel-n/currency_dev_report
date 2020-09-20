eb96f79cb8564bdb7fa87527d18d9cc5c4021ece
Adel Navracsics <navracsics.adel@gmail.com>
2020-09-20 12:21:44 +0200

    remove byebug, set date

--- app/controllers/currency_reports_controller.rb
+++ app/controllers/currency_reports_controller.rb
@@ -1,4 +1,7 @@
-class CurrencyReportsController < ApplicationController
+class CurrencyReportsController < ActionController::Base
+	include ActionView::Layouts
+  include ActionController::Rendering
+  append_view_path "#{Rails.root}/app/views"
 
 	def get_currency_reports
 		currency = params[:currency]
@@ -6,23 +9,35 @@ class CurrencyReportsController < ApplicationController
 		if params[:type] == "csv"
 			generate_csv
 		elsif params[:type] == "xls"
-
+			generate_xls
 		elsif params[:type] == "json"
 			render json: @rates
 		elsif params[:type] == "html"
-
+			render "get_currency_reports"
 		end
 	end
 
+	private
 
 	def generate_csv
-		CSV.generate do |csv|
-			csv << ["Date": Date.today]
-			csv << ["Rate", "Date", "Delta"]
-			@rates["data"].each do |d|
-				byebug
-				csv << d[1]
-			end
+		CSV.open("currency_dev_report.csv", "w") do |csv|
+			get_headers_and_rows(csv)
+		end
+	end
+
+	def generate_xls
+		CSV.open("currency_dev_report.xls", "wb") do |csv|
+			get_headers_and_rows(csv)
+		end
+	end
+
+	def get_headers_and_rows(csv)
+		#csv << ["Date: #{Date.today}"]
+		csv << ["2020-09-19"]
+		csv << ["Currency: EUR - #{@rates["currency"]}"]
+		csv << ["Rate", "Date", "Delta"]
+		@rates["data"].each do |d|
+			csv << d[1]
 		end
 	end
 end
--- app/models/currency.rb
+++ app/models/currency.rb
@@ -3,18 +3,20 @@ class Currency < ApplicationRecord
 	validates_presence_of :rate, :date
 
 	def self.calculate_rates(currency)
-		rate_today = Currency.find_by(code: currency, date: Date.today).rate
-		rate_yesterday = Currency.find_by(code: currency, date: Date.today - 1.days).rate
-		rate_week_ago = Currency.find_by(code: currency, date: Date.today - 1.weeks).rate
-		rate_month_ago = Currency.find_by(code: currency, date: Date.today - 1.months).rate
-		rate_year_ago = Currency.find_by(code: currency, date: Date.today - 1.years).rate
+		date = "2020-09-19".to_date
+		rate_today = Currency.find_by(code: currency, date: date).rate
+		rate_yesterday = Currency.find_by(code: currency, date: date - 1.days).rate
+		rate_week_ago = Currency.find_by(code: currency, date: date - 1.weeks).rate
+		rate_month_ago = Currency.find_by(code: currency, date: date - 1.months).rate
+		rate_year_ago = Currency.find_by(code: currency, date: date - 1.years).rate
 		rate_hash = {}
+		rate_hash["currency"] = currency
 		rate_hash["data"] = {}
-		rate_hash["data"]["today"] = [rate_today, (Date.today).to_time, 0]
-		rate_hash["data"]["yesterday"] = [rate_yesterday, (Date.today - 1.days).to_time, rate_today - rate_yesterday]
-		rate_hash["data"]["week_ago"] = [rate_week_ago, (Date.today - 1.weeks).to_time, rate_today - rate_week_ago]
-		rate_hash["data"]["month_ago"] = [rate_month_ago, (Date.today - 1.months).to_time, rate_today - rate_month_ago]
-		rate_hash["data"]["year_ago"] = [rate_year_ago, (Date.today - 1.years).to_time, rate_today - rate_year_ago]
+		rate_hash["data"]["today"] = [rate_today, date.to_time, 0]
+		rate_hash["data"]["yesterday"] = [rate_yesterday, (date - 1.days).to_time, rate_today - rate_yesterday]
+		rate_hash["data"]["week_ago"] = [rate_week_ago, (date - 1.weeks).to_time, rate_today - rate_week_ago]
+		rate_hash["data"]["month_ago"] = [rate_month_ago, (date - 1.months).to_time, rate_today - rate_month_ago]
+		rate_hash["data"]["year_ago"] = [rate_year_ago, (date - 1.years).to_time, rate_today - rate_year_ago]
 		rate_hash
 	end
 end
